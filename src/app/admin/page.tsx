dbd1408db2c02018bb2073f2d277a8fd486aa6fd
Tomas Ciernik <tomsky@email.com>
2024-07-08 14:01:58 +0100

    currency formatter and getters for data from db added

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,15 +5,84 @@ import {
   CardHeader,
   CardTitle,
 } from "@/src/components/ui/card";
+import db from "@/src/db/db";
+import { formatCurrency, formatNumber } from "../../lib/formatters";
 
-function getSalesData() {
-  
+async function getSalesData() {
+  const data = await db.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true,
+  });
+
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count,
+  };
 }
 
-export default function AdminDashboard() {
+async function getUserData() {
+  // 2 awaits are slow so better to have promise
+
+  // const userCount = await db.user.count()
+  // const orderData = await db.order.aggregate({
+  //   _sum: {pricePaidInCents: true},
+  // })
+
+  const [userCount, orderData] = await Promise.all([
+    db.user.count(),
+    db.order.aggregate({
+      _sum: { pricePaidInCents: true },
+    }),
+  ]);
+
+  return {
+    userCount,
+    averaValuePerUSer:
+      userCount === 0 ? 0 : (orderData._sum.pricePaidInCents || 0) / userCount /100,
+  };
+}
+
+async function getProductData() {
+  // db.product.count({where: {isAvailableForPurchase: true}})
+  // db.product.count({where: {isAvailableForPurchase: false}})
+
+  const [activeCount, inactiveCount ] = await Promise.all([
+    db.product.count({where: {isAvailableForPurchase: true}}),
+    db.product.count({where: {isAvailableForPurchase: false}})
+  ]);
+
+  return {activeCount, inactiveCount}
+}
+
+export default async function AdminDashboard() {
+  // again use Promise for better performance, rather than 2x await
+  // const salesData = await getSalesData();
+  // const userData = await getUserData();
+
+  const [ salesData, userData, productData ] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData()
+  ]);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <DashboardCard title="Sales" subtitle="Test" body="body" />
+      <DashboardCard
+        title="Sales"
+        subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
+        body={formatCurrency(salesData.amount)}
+      />
+      <DashboardCard
+        title="Customers"
+        subtitle={`${formatCurrency(userData.averaValuePerUSer)} Average Value`}
+        body={formatNumber(userData.userCount)}
+      />
+
+      <DashboardCard
+        title="Active Products"
+        subtitle={`${formatNumber(productData.inactiveCount)} Inactive`}
+        body={formatNumber(productData.activeCount)}
+      />
     </div>
   );
 }
