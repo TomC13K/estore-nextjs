bb200316f7d67926b7536ff905733a599c52cf36
Tomas Ciernik <tomsky@email.com>
2024-07-26 09:31:46 +0100

    fixed config with shadui and dropdown menu added

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,38 +1,91 @@
 import { Button } from "@/components/ui/button";
 import { PageHeader } from "../_components/PageHeader";
-import  Link  from "next/link";
-import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import db from "@/db/db";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+import { MoreVertical } from "lucide-react";
+import { DropdownMenu } from "@/components/ui/dropdown-menu";
+
 
 export default function AdminProductsPage() {
-    return (
+  return (
     <>
-    <div className="flex justify-between items-center gap-4">
+      <div className="flex justify-between items-center gap-4">
         <PageHeader>Products</PageHeader>
         <Button>
-            <Link href="/admin/products/new">Add Product</Link>
+          <Link href="/admin/products/new">Add Product</Link>
         </Button>
-    </div>
-    <ProductsTable/>  
+      </div>
+      <ProductsTable />
     </>
-    )
+  );
 }
 
-function ProductsTable() {
-    return (
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-0">
-              <span className="sr-only">Available For Purchase</span>
-            </TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead>Price</TableHead>
-            <TableHead>Orders</TableHead>
-            <TableHead className="w-0">
-              <span className="sr-only">Actions</span>
-            </TableHead>
+async function ProductsTable() {
+  const products = await db.product.findMany({
+    select: {
+      id: true,
+      name: true,
+      priceInCents: true,
+      isAvailableForPurchase: true,
+      _count: { select: { orders: true } },
+    },
+    orderBy: { name: "asc" },
+  });
+
+  if (products.length === 0) return <p>No products found</p>;
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-0">
+            <span className="sr-only">Available For Purchase</span>
+          </TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {products.map((product) => (
+          <TableRow key={product.id}>
+            <TableCell>
+              {product.isAvailableForPurchase ? (
+                <>
+                  "✔️"
+                  <span className="sr-only">Available </span>
+                </>
+              ) : (
+                <>
+                  "❌"
+                  <span className="sr-only">Unavailable </span>
+                </>
+              )}
+            </TableCell>
+            <TableCell>{product.name}</TableCell>
+            <TableCell>{formatCurrency(product.priceInCents / 100)}</TableCell>
+            <TableCell>{formatNumber(product._count.orders)}</TableCell>
+            <TableCell>
+              <DropdownMenu>
+                <MoreVertical />
+                <span className="sr-only"> Actions</span>
+              </DropdownMenu>
+            </TableCell>
           </TableRow>
-        </TableHeader>
-      </Table>
-    );
+        ))}
+      </TableBody>
+    </Table>
+  );
 }
